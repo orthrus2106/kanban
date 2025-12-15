10d07ea3de78e843b0a942669273148a9b277227
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-12-15 11:09:36 +0200

    updated types

--- src/types/board.ts
+++ src/types/board.ts
@@ -1,3 +1,5 @@
+// Domain
+
 export interface Task {
   id: number;
   title: string;
@@ -13,8 +15,29 @@ export interface Column {
   tasks: Task[];
 }
 
+// Responses
+
 export interface GetColumnsResponse {
   columns: Column[];
+}
+
+export interface CreateColumnResponse {
+  column: Column;
+}
+
+export interface UpdateTaskResponse {
+  task: Task;
+}
+
+export interface UpdateColumnResponse {
+  column: Column;
+}
+
+// DTO
+
+export interface UpdateColumnDTO {
+  title?: string;
+  order?: number;
 }
 
 export interface CreateTaskDTO {
@@ -27,6 +50,21 @@ export interface CreateColumnDTO {
   title: string;
 }
 
-export interface CreateColumnResponse {
-  column: Column;
+export interface UpdateTaskDTO {
+  title?: string;
+  description?: string | null;
+  order?: number;
+  columnId?: number;
+}
+
+// ARGs
+
+export interface UpdateColumnArgs {
+  id: number;
+  data: UpdateColumnDTO;
+}
+
+export interface UpdateTaskArgs {
+  id: number;
+  data: UpdateTaskDTO;
 }
